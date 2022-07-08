81c85fcf52b34b1db48eb683c53b9607c9a071a0
Daniel Ha <bb13112001@gmail.com>
2022-07-08 17:57:34 +0700

    Add Navbar

---
+++ tennis-web/src/components/Navbar/NavbarElements.js
@@ -0,0 +1,62 @@
+import { FaBars } from 'react-icons/fa';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  background: #1e5c29;
+  height: 600px;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 70%;
+  z-index: 12;
+  background-image: url("https://picsum.photos/id/412/1920/600");
+`;
+
+export const NavLink = styled(Link)`
+  color: #ffffff;
+  display: flex;
+  align-items: flex-start;
+  padding: 0 1rem;
+  height: 10px;
+  cursor: pointer;
+
+  &:hover {
+    color: #6f6f6f;
+  }
+  &.active {
+    color: #000000;
+  }
+
+  font-family: futura-pt;
+  font-weight: 500;
+  font-style: normal;
+  font-size: 13px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  text-decoration: none;
+  line-height: 1em;
+`;
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #808080;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin-top: 5%;
+  margin-right: -24px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
