1e707786e6920775c16739c2ebe66e0736b01ad8
Daniel Ha <bb13112001@gmail.com>
2022-07-08 17:57:49 +0700

    Update App.js

--- tennis-web/src/App.js
+++ tennis-web/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/index';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Course from './pages/course';
+import Teams from './pages/team';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/course' element={<Course/>} />
+        <Route path='/team' element={<Teams/>} />
+      </Routes>
+    </Router>
   );
 }
 
