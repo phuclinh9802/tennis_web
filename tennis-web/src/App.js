83c9b08410e72cdfb5b2648889254db4f19f65d6
Daniel Ha <bb13112001@gmail.com>
2022-07-12 16:23:32 +0700

    Update App.js

--- tennis-web/src/App.js
+++ tennis-web/src/App.js
@@ -1,11 +1,10 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/index';
-import About from './pages/about';
 import Contact from './pages/contact';
 import Course from './pages/course';
-import Teams from './pages/team';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -15,10 +14,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact element={<Home />} />
-          <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/course' element={<Course />} />
-          <Route path='/team' element={<Teams />} />
         </Routes>
         <Footer />
 
