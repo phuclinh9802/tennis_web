13aed98fb9a231da827d6d01abeed5f73e86c09b
Daniel Ha <bb13112001@gmail.com>
2022-07-12 16:25:06 +0700

    update contentcard

--- tennis-web/src/components/ContentCard/ContentCard.js
+++ tennis-web/src/components/ContentCard/ContentCard.js
@@ -4,14 +4,13 @@ import "./ContentCard.css"
 function ContentCard({ content, title }) {
     return (
         <div class="card-container">
-            <div class="card"><a href="/about">
+            <div class="card"><a href="/">
                 <div class="card--display">
                     <h2>{title}</h2>
                 </div>
                 <div class="card--hover">
                     <h2>{title}</h2>
                     <p>{content}</p>
-                    <p className="link">Read more</p>
                 </div></a>
                 <div class="card--border"></div>
             </div>
