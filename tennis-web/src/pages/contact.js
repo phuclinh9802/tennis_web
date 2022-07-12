dea3b4d96e15fdc9981203f5e2ff3aa6b16807f4
Daniel Ha <bb13112001@gmail.com>
2022-07-12 16:24:23 +0700

    update contact style

--- tennis-web/src/pages/contact.js
+++ tennis-web/src/pages/contact.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Typography } from '@mui/material';
 import ContactForm from '../components/ContactForm/Contact';
 import GoogleMap from '../components/GoogleMap/GoogleMap';
