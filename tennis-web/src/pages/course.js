de8b5f0b6ed70f76b99daabbcc6ae03ee9f0b7d3
Daniel Ha <bb13112001@gmail.com>
2022-07-15 15:52:11 +0700

    Add playlist and tabs

--- tennis-web/src/pages/course.js
+++ tennis-web/src/pages/course.js
@@ -1,24 +1,11 @@
 import React from "react";
-import Carousel from "react-material-ui-carousel"
-import YoutubeEmbed from "../components/YoutubeEmbed/YoutubeEmbed"
+import BasicTabs from "../components/TabPanel/TabPanel";
 import "./styles/course.css"
 
 function Course() {
     return (
         <div className="video-container">
-            <div className="section">
-                <h2>Drill 1</h2>
-                <Carousel autoPlay={false} cycleNavigation = {false} duration = {1000}>
-                    <div className="video">
-                        <YoutubeEmbed embedId="HGl75kurxok" />
-                        <h3>Title</h3>
-                    </div>
-                    <div className="video">
-                        <YoutubeEmbed embedId="UpMJOf8jj8k" />
-                        <h3>Title</h3>
-                    </div>
-                </Carousel>
-            </div>
+            <BasicTabs />
         </div>
     );
 }
