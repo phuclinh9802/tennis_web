2483802b7490c48441bbda174a72247b353c6bee
Daniel Ha <bb13112001@gmail.com>
2022-07-29 17:25:48 +0700

    add imgs

--- tennis-web/src/components/Gallery/MainImages.js
+++ tennis-web/src/components/Gallery/MainImages.js
@@ -1,45 +1,165 @@
 export const MainImages =
     [
         {
-            src: "https://picsum.photos/800/?random=1",
-            thumbnail: "https://picsum.photos/800/?random=1",
+            src: process.env.PUBLIC_URL + "/images/IMG_0176.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0176.jpg",
+            thumbnailWidth: 220,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0221.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0221.jpg",
             thumbnailWidth: 320,
-            thumbnailHeight: 174,
+            thumbnailHeight: 220,
         },
         {
-            src: "https://picsum.photos/500/?random=2",
-            thumbnail: "https://picsum.photos/500/?random=2",
-            thumbnailWidth: 500,
-            thumbnailHeight: 212,
-        },
-        {
-            src: "https://picsum.photos/500/?random=3",
-            thumbnail: "https://picsum.photos/500/?random=3",
+            src: process.env.PUBLIC_URL + "/images/IMG_0223.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0223.jpg",
             thumbnailWidth: 320,
-            thumbnailHeight: 500
+            thumbnailHeight: 220,
         },
         {
-            src: "https://picsum.photos/500/?random=4",
-            thumbnail: "https://picsum.photos/500/?random=4",
-            thumbnailWidth: 450,
-            thumbnailHeight: 300
-        },
-        {
-            src: "https://picsum.photos/500/?random=5",
-            thumbnail: "https://picsum.photos/500/?random=5",
-            thumbnailWidth: 500,
-            thumbnailHeight: 212
-        },
-        {
-            src: "https://picsum.photos/500/?random=6",
-            thumbnail: "https://picsum.photos/500/?random=6",
+            src: process.env.PUBLIC_URL + "/images/IMG_0224.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0224.jpg",
             thumbnailWidth: 320,
-            thumbnailHeight: 212
+            thumbnailHeight: 220,
         },
         {
-            src: "https://picsum.photos/800/?random=7",
-            thumbnail: "https://picsum.photos/800/?random=7",
+            src: process.env.PUBLIC_URL + "/images/IMG_0225.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0225.jpg",
+            thumbnailWidth: 220,
+            thumbnailHeight: 320,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0226.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0226.jpg",
             thumbnailWidth: 320,
-            thumbnailHeight: 212
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0230.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0230.jpg",
+            thumbnailWidth: 220,
+            thumbnailHeight: 320,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0633.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0633.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0637.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0637.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0639.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0639.jpg",
+            thumbnailWidth: 220,
+            thumbnailHeight: 320,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0651.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0651.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0781.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0781.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_0913.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_0913.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_1322.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_1322.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_1924.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_1924.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_2117.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_2117.jpg",
+            thumbnailWidth: 220,
+            thumbnailHeight: 320,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_2148.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_2148.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_3630.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_3630.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_3791.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_3791.jpg",
+            thumbnailWidth: 220,
+            thumbnailHeight: 320,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_4677.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_4677.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_4757.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_4757.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_4760.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_4760.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_4761.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_4761.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_4771.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_4771.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_4932.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_4932.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 320,
+        },
+        {
+            src: process.env.PUBLIC_URL + "/images/IMG_5016.jpg",
+            thumbnail: process.env.PUBLIC_URL + "/images/IMG_5016.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 220,
         },
     ]
+
+export function getMainImages() {
+    return new Promise((resolve) => {
+        resolve(MainImages)
+    })
+}
