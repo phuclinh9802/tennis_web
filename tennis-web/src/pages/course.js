c42f7e46e963c2eb39466a9d4478389a8fe9a0ca
Daniel Ha <bb13112001@gmail.com>
2022-08-02 10:49:22 +0700

    update course page

--- tennis-web/src/pages/course.js
+++ tennis-web/src/pages/course.js
@@ -8,7 +8,7 @@ function Course() {
         <>
             <div className="course">
                 <div className="img-container">
-                    <Carousel height="400px" indicators="false">
+                    <Carousel height="400px" indicators={false}>
                         <img src="https://picsum.photos/id/69/1920" className="img-card" alt="img not found" />
                         <img src="https://picsum.photos/id/70/1920" className="img-card" alt="img not found" />
                         <img src="https://picsum.photos/id/71/1920" className="img-card" alt="img not found" />
@@ -18,16 +18,16 @@ function Course() {
                     <h1>Course A</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                 </div>
-                <div className="content-container">
-                    <h1>Course B</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                </div>
                 <div className="img-container">
-                    <Carousel height="400px">
+                    <Carousel height="400px" indicators={false}>
                         <img src="https://picsum.photos/id/72/1920" className="img-card" alt="img not found" />
                         <img src="https://picsum.photos/id/73/1920" className="img-card" alt="img not found" />
                         <img src="https://picsum.photos/id/74/1920" className="img-card" alt="img not found" />
                     </Carousel>
+                </div>
+                <div className="content-container">
+                    <h1>Course B</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                 </div>
             </div>
             <div className="video-container">
