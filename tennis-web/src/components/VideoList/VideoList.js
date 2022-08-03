d7e18ce54652987bb2cc43c6cc267d4f5ea3b2fe
Daniel Ha <bb13112001@gmail.com>
2022-08-03 10:39:04 +0700

    add video tabs

--- tennis-web/src/components/VideoList/VideoList.js
+++ tennis-web/src/components/VideoList/VideoList.js
@@ -1,16 +1,30 @@
-import React from "react";
+export const KidDrill = [
+    { id: "OypXKR_4b2Q", title: "Buổi đầu nhập môn cho trẻ" },
+    { id: "V-1g_Imx4pI", title: "Huấn luyện tennis cơ bản cho trẻ" },
+    { id: "pZM3uyhzvFE", title: "Bài tập phát triển thể lực cho trẻ" },
+    { id: "XJT5gIrqAN4", title: "Tennis cho trẻ" },
+    { id: "pI7cL2v0gXw", title: "Huấn luyện tennis cho trẻ" },
+    { id: "EyY5XeVyoMo", title: "Rèn luyện độ nảy của chân" },
+    { id: "iv1LCQI4pNE", title: "3 bạn nhỏ đáng yêu" },
+]
 
-function VideoList({ playlistId }) {
-    return (
-        <iframe
-            id="ytplayer"
-            className="video"
-            type="text/html"
-            src={`https://www.youtube.com/embed?listType=playlist&list=${playlistId}`}
-            frameborder="0"
-            title="youtubeplaylist"
-        />
-    )
-}
+export const TeenDrill = [
+    { id: "NcKiVwsWCtg", title: "Buổi tập di chuyển đánh thuận tay" },
+    { id: "aKWmVdc1S8Q", title: "Buổi tập đánh trái 1 tay của học viên" },
+    { id: "w7HmEqJ8Zcw", title: "Hướng dẫn tennis cơ bản cho nữ" },
+    { id: "Ai2c4ESXO18", title: "Đánh bóng nhịp nhàng và ổn định" },
+    { id: "OKI78M9r2D4", title: "Buổi tập phát bóng của học viên" },
+    { id: "Yyz2FFMM2uE", title: "Buổi tập trái 1 tay của học viên" },
+]
 
-export default VideoList;
+export const ExtraDrill = [
+    { id: "BYnxjs0689Y", title: "Kĩ thuật xử lí bóng thấp-bóng nằm trong sân" },
+    { id: "H5cffua-orc", title: "Bài tập di chuyển ngang không bóng" },
+    { id: "wj_DvMdqGVw", title: "Bài tập giúp bộ chân linh hoạt" },
+    { id: "VpbpR4MHV4k", title: "Bài tập nâng cao thể lực trong tennis" },
+    { id: "FFy7Ole4FjU", title: "Bài tập phát triển tốc độ" },
+    { id: "z3eZcX4MVqs", title: "Bài tập nhịp cho cú thuận tay tennis" },
+    { id: "2v_OxbCo9E4", title: "Bài tập tăng cảm giác tạo lực trong kĩ thuật đánh trái 2 tay" },
+    { id: "iyBpzQ8OXdA", title: "Bài tập giúp tăng thể lực và phản xạ bóng" },
+    { id: "cWWWed1uFD8", title: "Bài tập phom đánh" },
+]
